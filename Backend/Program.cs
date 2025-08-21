594f276939ded8514df685c068217a89aee1fa64
Tran Tuan Kiet <tuankiet061203@gmail.com>
2025-08-21 21:57:07 +0700

    Adjusted CORS

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -17,13 +17,22 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddCors(opt =>
+//builder.Services.AddCors(opt =>
+//{
+//    opt.AddPolicy("fe", p => p
+//        .AllowAnyHeader()
+//        .AllowAnyMethod()
+//        .AllowCredentials()
+//        .WithOrigins("http://localhost:5173", "https://your-fe-domain.vercel.app"));
+//});
+builder.Services.AddCors(options =>
 {
-    opt.AddPolicy("fe", p => p
-        .AllowAnyHeader()
-        .AllowAnyMethod()
-        .AllowCredentials()
-        .WithOrigins("http://localhost:5173", "https://your-fe-domain.vercel.app"));
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("https://inventory-frontend-tran-tuan-kiet.vercel.app")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
 });
 
 var jwt = builder.Configuration.GetSection("Jwt");
